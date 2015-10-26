c576c13b7b597632ac8aea1b322eff00ade85e14
enbandari <305779913@qq.com>
2015-10-26 10:39:52 +0800

    update scripts

--- code/kt07/src/main/java/net/println/kt07/Lambda.kt
+++ code/kt07/src/main/java/net/println/kt07/Lambda.kt
@@ -2,21 +2,22 @@ package net.println.kt07
 
 import rx.Observable
 import java.io.File
+import java.util.concurrent.Executors
 
 fun main(args: Array<String>) {
-    //1. 统计单词的个数
-    //2. 统计字母的个数
-    val text = File(ClassLoader.getSystemClassLoader().getResource("input").path).readText()
-    println(text)
-
-    Observable.from(text.split(Regex("""\s"""))).filter{
-        it.matches(Regex("""[a-zA-z,.'-]+"""))
-    }.flatMap { Observable.from(it.toCharArray().asIterable()) }.groupBy { it }.subscribe{
-        observable ->observable.count().subscribe{
-            println("${observable.key} -> $it")
+    val text = File(ClassLoader.getSystemResource("input").path).readText()
+    Observable.from(text.toCharArray().asIterable()).filter { !it.isWhitespace() }.groupBy { it }.map {
+        o -> o.count().subscribe{
+            println("${o.key} -> $it")
         }
+    }.subscribe()
+
+    val worker = Executors.newCachedThreadPool()
+    worker.execute {
+        println("Hello")
     }
 }
+
 
 
 
