16ead504a97a8d568ce043553a985da055c748b4
enbandari <305779913@qq.com>
2016-10-26 10:55:13 +0800

    update scripts

--- code/kt07/src/main/java/net/println/kt07/Lambda.kt
+++ code/kt07/src/main/java/net/println/kt07/Lambda.kt
@@ -6,11 +6,11 @@ import java.util.concurrent.Executors
 
 fun main(args: Array<String>) {
     val text = File(ClassLoader.getSystemResource("input").path).readText()
-    Observable.from(text.toCharArray().asIterable()).filter { !it.isWhitespace() }.groupBy { it }.map {
+    Observable.from(text.toCharArray().asIterable()).filter { !it.isWhitespace() }.groupBy { it }.subscribe {
         o -> o.count().subscribe{
             println("${o.key} -> $it")
         }
-    }.subscribe()
+    }
 
     val worker = Executors.newCachedThreadPool()
     worker.execute {
