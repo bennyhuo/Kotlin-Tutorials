d94f5e03ea0b19d8bb23b3cd88ee438b7ff52228
enbandari <305779913@qq.com>
2016-11-02 20:54:50 +0800

    Update script

--- code/Kt08/src/main/java/net/println/kt08/Service.kt
+++ code/Kt08/src/main/java/net/println/kt08/Service.kt
@@ -4,24 +4,22 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 /**
  * Created by benny on 11/1/16.
  */
 interface GitHubService{
 
-    @GET("/repos/{user}/{repo}/stargazers")
-    fun getStarGazers(@Path("user") user: String, @Path("repo") repo: String): Call<List<User>>
+    @GET("/repos/enbandari/Kotlin-Tutorials/stargazers")
+    fun getStarGazers(): Call<List<User>>
 }
 
 object Service{
-    val gitHub: GitHubService by lazy {
+    val gitHubService: GitHubService by lazy {
         Retrofit.Builder().baseUrl("https://api.github.com").addConverterFactory(GsonConverterFactory.create()).build().create(GitHubService::class.java)
     }
 }
 
 fun main(args: Array<String>) {
-    Service.gitHub.getStarGazers("enbandari", "Kotlin-Tutorials").execute().body().map(::println)
+    Service.gitHubService.getStarGazers().execute().body().map(::println)
 }
-
--- code/Kt08/src/main/java/net/println/kt08/User.kt
+++ code/Kt08/src/main/java/net/println/kt08/User.kt
@@ -3,27 +3,8 @@ package net.println.kt08
 /**
  * Created by benny on 11/1/16.
  */
-
-/**
- * {
-login: "bindy",
-id: 849661,
-avatar_url: "https://avatars.githubusercontent.com/u/849661?v=3",
-gravatar_id: "",
-url: "https://api.github.com/users/bindy",
-html_url: "https://github.com/bindy",
-followers_url: "https://api.github.com/users/bindy/followers",
-following_url: "https://api.github.com/users/bindy/following{/other_user}",
-gists_url: "https://api.github.com/users/bindy/gists{/gist_id}",
-starred_url: "https://api.github.com/users/bindy/starred{/owner}{/repo}",
-subscriptions_url: "https://api.github.com/users/bindy/subscriptions",
-organizations_url: "https://api.github.com/users/bindy/orgs",
-repos_url: "https://api.github.com/users/bindy/repos",
-events_url: "https://api.github.com/users/bindy/events{/privacy}",
-received_events_url: "https://api.github.com/users/bindy/received_events",
-type: "User",
-site_admin: false
-},
- */
-
-data class User(val login: String, val id: Long, val avatar_url: String, val url: String)
+data class User(val login: String, val id: Long, val avatar_url: String){
+    override fun toString(): String {
+        return login
+    }
+}
